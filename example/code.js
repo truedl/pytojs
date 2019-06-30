f38b4e11e4af91a636b2d535dfbe725fce86e1fa
Tom <25082829+truedl@users.noreply.github.com>
2019-06-30 22:39:04 +0300

    code.py -> code.js

---
+++ example/code.js
@@ -0,0 +1,11 @@
+console.log('Hello, World!');
+var x = 10;
+console.log(x);
+if(x == 10){
+	console.log('x is 10');
+	if(1){
+		console.log('x');
+	}
+}
+var y = 20;
+var z = 30;
---
+++ example/code.py
@@ -0,0 +1,11 @@
+print('Hello, World!')
+x = 10
+print(x)
+
+if x == 10:
+    print('x is 10')
+    if 1:
+        print('x')
+
+y = 20
+z = 30
